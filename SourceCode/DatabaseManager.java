8af80ce56a1735398f0a0daafd4703c8647efeb6
Rj-99 <rahuljham99@gmail.com>
2020-05-11 07:43:11 +0530

    Added Post Time Card Feature

--- SourceCode/DatabaseManager.java
+++ SourceCode/DatabaseManager.java
@@ -4,7 +4,7 @@ interface DatabaseManager
 {
     void addEmployee();
     void DeleteEmployee(int ID);
-    // void PostTimeCard(int ID, TimeCard card);
+    void PostTimeCard(int ID, TimeCard card);
     // void postSalesReceipt(int ID, SalesReceipt receipt);
     // void postUnionCharges(double charges);
     // void changeDetails(int ID);
